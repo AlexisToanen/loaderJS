e039b1e6b30bed89b7460f98b66b6efaea5a9ebb
AlexisToanen <alexistoanen@gmail.com>
2020-04-23 08:02:19 +0200

    loader JS WIP

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,6 @@
-/*TODO*/
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+    const stopload = this.document.querySelector(".spinner");
+    stopload.classList.add("stop");
+});
